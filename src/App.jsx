2cb8fd69e37f29a3cbacca2465ce6353f31bc49c
Josh <joshboniaoxd@gmail.com>
2026-09-21 20:42:45 +0800

    1.3: course information step 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,24 @@
 
 function App ()  {
+  const course = 'BSIT'
   const name = 'Josh C. Boniao'
   const section = 'G8'
-  const course ='BSIT'
-  const part1 = 'Industry Elective'
-  const exercises1 = 3
-  const part2 = 'Data Structures'
-  const exercises2 = 3
-  const part3 = 'Application Development'
-  const exercises3 = 3
-  const Part = ({part, exercise}) => {
-    return(
-    <div>
-        <p>
-        {part}: {exercise} Units
-        </p>
-    </div>
-    )
+  const Part1 = {
+    name: 'Industry Elective',
+    units: 3
+  }
+  const Part2 = {
+    name: 'Data Structures',
+    units: 3
+  }
+  const Part3 = {
+    name: 'Application Development',
+    units: 3
   }
 
-  const Header = ({course}) => {
-  return (
-    <div>
+  const total = Part1.units + Part2.units + Part3.units
 
-      <p>{course}</p>
-    </div>
-  )
-}
-  const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
-  return (
-    <div>
-        <Part part = {part1} exercise = {exercises1}></Part>
-        <Part part = {part2} exercise = {exercises2}></Part>
-        <Part part = {part3} exercise = {exercises3}></Part>
-
-    </div>
-  )
-}
-const Footer = ({ name, course, section }) => {
+  const Footer = ({ name, course, section }) => {
   return (
     <footer>
       {name} - {course} - {section}
@@ -45,20 +26,21 @@ const Footer = ({ name, course, section }) => {
   )
 }
 
+
+
   return (
-    <>
     <div>
-      <Header course = {course}/>
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 ={exercises2} exercises3 = {exercises3}/>
-      <div>
-        
-        
-        <p>Number of Units {exercises1 + exercises2 + exercises3}</p>
-      </div>
-      <Footer name = {name} course = {course} section = {section}></Footer>
+      <h1><b>Course:</b> {course}</h1>
+      <p>{Part1.name} - {Part1.units} Units </p>
+      <p>{Part2.name} - {Part1.units} Units </p>
+      <p>{Part3.name} - {Part1.units} Units </p>
+      <p>Total Amount of Units: {total}</p>
+
+      <hr/>
+      <Footer name = {name} course = {course} section = {section}/>
     </div>
-    </>
   )
+
 }
 
 export default App
